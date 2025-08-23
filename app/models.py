6f57fe9035c4633b385b8b7babea0bbb48b30ad5
Devom Brahmbhatt <Devom.b@yahoo.com>
2025-08-23 11:25:32 -0700

    New DB Schema
    Work on Proprietary ORM

--- app/models.py
+++ app/models.py
@@ -1,2 +1,13 @@
 from enum import Enum
+from pydantic import BaseModel
 
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+
+class Patient(BaseModel):
+    id: int
+    name: str
+    age: int
+    gender: Gender
+    
